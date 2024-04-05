6cc9061e441e99dfccbb48ac8ca93fcd428f270a
akashsuu <94668360+akashsuu@users.noreply.github.com>
2024-04-05 09:32:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+  // Function to generate and append badge
+        function generateBadge() {
+            // Replace these placeholders with actual project details
+            var badgeText = "Build: Passing";
+            var badgeColor = "green";
